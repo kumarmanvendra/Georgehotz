3549614b83c21ae6f9a5a86e1ea5762cf3be7acb
thelonejordan <jyotirmaya.mahanta@gmail.com>
2024-10-30 00:38:43 +0530

    bugfix

--- tinygrad/shape/view.py
+++ tinygrad/shape/view.py
@@ -207,6 +207,7 @@ class View:
         idx = 0
         if len(term) == 0:
           if newe: newe[0] = 0
+          bad = True
           break
         elif len(term) > 1:
           if not all_int([t[1] for t in term]):
